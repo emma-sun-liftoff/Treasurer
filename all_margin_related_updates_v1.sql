da5e211801406b733a1042debb42cddae738f968
Emma Sun <97474101+emma-sun-liftoff@users.noreply.github.com>
2023-05-25 18:38:19 -0400

    Update all_margin_related_updates_v1.sql

--- all_margin_related_updates_v1.sql
+++ all_margin_related_updates_v1.sql
@@ -1,11 +1,17 @@
  --- below it is the base query
  --  we can find all NR/RPI campaigns’ update dates when we 1) switch targets 2) apply manual adjustment 3)apply any system-wide margin update 4)when we change margin exploration algorithm. and we can find all disabled campaigns
+ --  all campaigns are enabled campaigns after 2023/01/01. Any campaigns that are paused after 01/01 will be excluded.
  --  how to use each columns as filter:
  --  num_target is a good way to help you find campaigns where targets have been changed. Please reach out to me if you find num_target = 0.
  --  updated_source is a filter to find manual update (=skipper), treasurer update (=treasurer), and eng-driven adhoc update (=adhoc).
  -- 
 
-WITH all_ AS (    
+
+
+WITH para AS (
+SELECT '2023-04-05' AS start_dt)
+
+, all_ AS (    
 SELECT 
 ctc.campaign_id
 , tda.target AS tda_target
@@ -23,8 +29,9 @@ WHERE ctc.campaign_id IN (
   DISTINCT ctc.campaign_id
   FROM pinpoint.public.campaign_treasurer_configs ctc
   FULL OUTER JOIN pinpoint.public.elephant_changes ec ON  ctc.id = ec.row_id
+  CROSS JOIN para
   WHERE ec.table_name = 'campaign_treasurer_configs'
-  	AND ec.logged_at > from_iso8601_timestamp('2023-04-05')
+  	AND ec.logged_at > from_iso8601_timestamp(start_dt)
   	--AND ctc.campaign_id IN (4293, 12624, 29288,6318)  
  )	
 GROUP BY 1,2,3,4,5,6
@@ -54,7 +61,8 @@ FROM (SELECT
     AND ec."source" <> 'treasurer'
     ORDER BY 2 DESC     
 ) AS TT
-WHERE logged_at > from_iso8601_timestamp('2023-04-05')
+CROSS JOIN para 
+WHERE logged_at > from_iso8601_timestamp(start_dt)
 GROUP BY 1,2,3,4,5,6
 
 UNION ALL 
@@ -64,15 +72,16 @@ UNION ALL
   ctc.campaign_id
     , CAST(json_extract_scalar(ec.new_values, '$.target') AS varchar) AS tda_target
    ,  ctc.target_reasons AS target_reasons
-    , NULL AS update_source
+    , 'target_switch' AS update_source
     , NULL AS margin_reason
     , NULL AS margin_exploration_algorithm 
    , ec.logged_at AS min_updated_at
   FROM pinpoint.public.campaign_treasurer_configs ctc
   FULL OUTER JOIN pinpoint.public.elephant_changes ec ON  ctc.id = ec.row_id
   WHERE ec.table_name = 'campaign_treasurer_configs'
-  AND CAST(json_extract(ec.new_values, '$.target') AS varchar)= 'disabled' 
-  AND ec.logged_at > from_iso8601_timestamp('2023-04-05')
+  --AND CAST(json_extract(ec.new_values, '$.target') AS varchar)= 'disabled' 
+  AND json_extract(ec.new_values, '$.target') IS NOT NULL 
+  --AND ec.logged_at > from_iso8601_timestamp('2023-04-05')
   AND ctc.campaign_id NOT IN (SELECT
 	DISTINCT row_id AS campaign_id
 	FROM pinpoint.public.elephant_changes
