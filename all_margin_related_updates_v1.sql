62b6b2f1f7cf8699ba0a0ead78b8e78fa602622e
Emma Sun <97474101+emma-sun-liftoff@users.noreply.github.com>
2023-05-25 19:30:08 -0400

    Update all_margin_related_updates_v1.sql

--- all_margin_related_updates_v1.sql
+++ all_margin_related_updates_v1.sql
@@ -1,11 +1,11 @@
  --- below it is the base query
  --  we can find all NR/RPI campaigns’ update dates when we 1) switch targets 2) apply manual adjustment 3)apply any system-wide margin update 4)when we change margin exploration algorithm. and we can find all disabled campaigns
  --  all campaigns are enabled campaigns after 2023/01/01. Any campaigns that are paused after 01/01 will be excluded.
+ --  all nrm are final version
  --  how to use each columns as filter:
  --  num_target is a good way to help you find campaigns where/when targets have been changed. This count all target changes since 2022/12/08. Please reach out to me if you find num_target = 0.
  --  updated_source is a filter to find manual update (=skipper), treasurer update (=treasurer), eng-driven adhoc update (=adhoc), and target update (=target_switch).
  -- 
-
 WITH para AS (
 -- Please change the para below to specify the time frame
 SELECT '2023-04-05' AS start_dt
@@ -19,6 +19,10 @@ ctc.campaign_id
 , 'treasurer' AS update_source
 , tda.margin_reason 
 , tda.margin_exploration_algorithm 
+, max(CAST(tda.new_control_gross_margin_vungle AS double)/(1 + CAST(tda.new_control_gross_margin_vungle AS double))) AS new_ctrl_vungle_nrm
+, max(CAST(tda.new_control_gross_margin_non_vungle AS double)/(1 + CAST(tda.new_control_gross_margin_non_vungle AS double))) AS new_ctrl_non_vungle_nrm
+, max(CAST(tda.new_experiment_gross_margin_vungle AS double)/(1 + CAST(tda.new_experiment_gross_margin_vungle AS double))) AS new_exp_vungle_nrm
+, max(CAST(tda.new_experiment_gross_margin_non_vungle AS double)/(1 + CAST(tda.new_experiment_gross_margin_non_vungle AS double))) AS new_exp_non_vungle_nrm
 , min(tda.updated_at) AS min_update_at
 FROM pinpoint.public.campaign_treasurer_configs ctc
 LEFT JOIN ml_adops.public.treasurer_daily_analysis tda 
@@ -33,7 +37,6 @@ WHERE ctc.campaign_id IN (
   WHERE ec.table_name = 'campaign_treasurer_configs'
   	AND ec.logged_at >= from_iso8601_timestamp(start_dt)
   	AND ec.logged_at < from_iso8601_timestamp(end_dt)
-  	--AND ctc.campaign_id IN (4293, 12624, 29288,6318)  
  )	
 GROUP BY 1,2,3,4,5,6
 
@@ -46,6 +49,10 @@ campaign_id
 , "source" AS update_source
 , NULL AS margin_reason
 , NULL AS margin_exploration_algorithm
+, max(CASE WHEN margin_type = 'control' THEN CAST(new_vungle_gross_margin AS double)/(1 + CAST(new_vungle_gross_margin AS double)) END) AS new_ctrl_vungle_nrm
+, max(CASE WHEN margin_type = 'control' THEN CAST(new_non_vungle_gross_margin AS double)/(1 + CAST(new_non_vungle_gross_margin AS double)) END) AS new_ctrl_non_vungle_nrm
+, max(CASE WHEN margin_type <> 'control' THEN CAST(new_vungle_gross_margin AS double)/(1 + CAST(new_vungle_gross_margin AS double)) END) AS new_exp_vungle_nrm
+, max(CASE WHEN margin_type <> 'control' THEN CAST(new_non_vungle_gross_margin AS double)/(1 + CAST(new_non_vungle_gross_margin AS double)) END) AS new_exp_non_vungle_nrm
 , min(logged_at) AS min_updated_at
 FROM (SELECT 
     tm.campaign_id
@@ -72,12 +79,16 @@ UNION ALL
 
   SELECT 
   ctc.campaign_id
-    , CAST(json_extract_scalar(ec.new_values, '$.target') AS varchar) AS tda_target
-   ,  ctc.target_reasons AS target_reasons
-    , 'target_switch' AS update_source
+	, CAST(json_extract_scalar(ec.new_values, '$.target') AS varchar) AS tda_target
+	,  ctc.target_reasons AS target_reasons
+	, 'target_switch' AS update_source
     , NULL AS margin_reason
     , NULL AS margin_exploration_algorithm 
-   , ec.logged_at AS min_updated_at
+    , NULL AS new_ctrl_vungle_nrm
+    , NULL AS new_ctrl_non_vungle_nrm
+    , NULL AS new_exp_vungle_nrm
+    , NULL AS new_exp_non_vungle_nrm
+    , ec.logged_at AS min_updated_at
   FROM pinpoint.public.campaign_treasurer_configs ctc
   FULL OUTER JOIN pinpoint.public.elephant_changes ec ON  ctc.id = ec.row_id
   WHERE ec.table_name = 'campaign_treasurer_configs'
@@ -103,6 +114,10 @@ SELECT all_.campaign_id
 , update_source
 , margin_reason
 , margin_exploration_algorithm
+, new_ctrl_vungle_nrm
+, new_ctrl_non_vungle_nrm
+, new_exp_vungle_nrm
+, new_exp_non_vungle_nrm
 FROM all_
 LEFT JOIN (SELECT campaign_id, count(DISTINCT tda_target) AS num_target FROM all_ GROUP BY 1) AS num_target ON all_.campaign_id = num_target.campaign_id
 WHERE all_.campaign_id NOT IN (SELECT
@@ -120,8 +135,6 @@ ORDER BY 1,2
 
 -- query examples 
 -- all disabled campaigns during a time frame (there are two cases: 1) disabled all the way 2) disabled after NR/RPI. If the former, num_target = 1)
--- campaigns 
--- copy and paste the first 97 line
 SELECT*
 FROM all1_
 WHERE tda_target = 'disabled'
