e4976f62e682c8eaa3dc8b5c17b9b1ab33e62447
Emma Sun <97474101+emma-sun-liftoff@users.noreply.github.com>
2023-05-26 16:27:58 -0400

    Update all_Treasurer_margin_updates.sql

--- all_Treasurer_margin_updates.sql
+++ all_Treasurer_margin_updates.sql
@@ -1,3 +1,44 @@
+-- we can leverage ml_adops.public.treasurer_daily_analysis table to pull all Treasurer margin updates
+SELECT
+campaign_id 
+, date_trunc('hour',updated_at) AS updated_at
+, target 
+, threshold 
+, margin_exploration_algorithm
+, margin_reason 
+, CASE WHEN old_control_gross_margin_vungle <> new_control_gross_margin_vungle 
+		    OR old_control_gross_margin_non_vungle <> new_control_gross_margin_non_vungle 
+	   THEN 'Yes' ELSE  'No'END AS ctrl_update
+, CASE WHEN old_experiment_gross_margin_vungle <> new_experiment_gross_margin_vungle 
+		    OR old_experiment_gross_margin_non_vungle <> new_experiment_gross_margin_non_vungle 
+	   THEN 'Yes' ELSE 'No'END AS test_update
+, ROUND((old_control_gross_margin_vungle)/(old_control_gross_margin_vungle + 1),3) AS old_ctrl_margin_vungle
+, ROUND((old_control_gross_margin_non_vungle)/(old_control_gross_margin_non_vungle + 1),3) AS old_ctrl_margin_non_vungle
+, ROUND((new_control_gross_margin_vungle)/(new_control_gross_margin_vungle + 1),3) AS new_ctrl_margin_vungle
+, ROUND((new_control_gross_margin_non_vungle)/(new_control_gross_margin_non_vungle + 1),3) AS new_ctrl_margin_non_vungle
+, ROUND((old_experiment_gross_margin_vungle)/(old_experiment_gross_margin_vungle + 1),3) AS old_test_margin_vungle
+, ROUND((old_experiment_gross_margin_non_vungle)/(old_experiment_gross_margin_non_vungle + 1),3) AS old_test_margin_non_vungle
+, ROUND((new_experiment_gross_margin_vungle)/(new_experiment_gross_margin_vungle + 1),3) AS new_test_margin_vungle
+, ROUND((new_experiment_gross_margin_non_vungle)/(new_experiment_gross_margin_non_vungle + 1),3) AS new_test_margin_non_vungle
+, measured_control_nrm AS ctrl_nrm
+, measured_experiment_nrm AS test_nrm
+, predicted_best_nrm 
+, predicted_best_rpi 
+, measured_control_rpi 
+, measured_experiment_rpi 
+, CASE WHEN measured_control_rpi IS NOT NULL THEN 'Yes' ELSE 'No' END AS ctrl_rpi_stat_sig
+, CASE WHEN measured_experiment_rpi IS NOT NULL THEN 'Yes' ELSE 'No' END AS test_rpi_stat_sig
+, CAST(control_gross_revenue AS DOUBLE)/1000000 AS cumulative_ctrl_gross_revenue 
+, control_installs AS cumulative_ctrl_installs 
+, CAST(experiment_gross_revenue AS DOUBLE)/1000000 AS cumulative_test_gross_revenue  
+, experiment_installs AS cumulative_test_installs 
+FROM ml_adops.public.treasurer_daily_analysis
+WHERE control_gross_revenue IS NOT NULL
+    AND experiment_gross_revenue IS NOT NULL
+
+
+
+-- we can use elephant_changes for double check.
 SELECT 
     tm.campaign_id
     , logged_at
